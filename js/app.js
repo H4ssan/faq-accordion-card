93b9331e56ebd7d3df117785438a352dca8499c0
H4ssan <hassan.ashraf98@gmail.com>
2020-11-13 21:46:03 +0000

    fix: solved issue with image layout

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const question = document.getElementsByClassName("question");
+
+
+function myFunction() {
+    const answer = document.getElementsByClassName("answer");
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+    } else {
+      answer.style.display = "none";
+    }
+  }
+
+  
