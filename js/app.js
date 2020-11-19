4dda35d7049b521a548fecbdeaa36e44b756b2ee
H4ssan <hassan.ashraf98@gmail.com>
2020-11-19 14:10:37 +0000

    fix: Accordion size no longer changes when clicked on

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const question = document.getElementsByClassName("question");
 for(let i=0;i<question.length;i++){
   question[i].addEventListener("click", function(){
     this.classList.toggle("active");
+    
+    
 
     const ans = this.nextElementSibling;
     if (ans.style.display === "block") {
